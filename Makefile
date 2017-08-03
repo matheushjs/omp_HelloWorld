f133bd603995d429712c9c50bb86e3eb40bd5d85
Matheus Saldanha <matheus.saldanha@usp.br>
2017-08-02 22:49:18 -0300

    Implement vecsum

--- Makefile
+++ Makefile
@@ -1,6 +1,9 @@
-all: locvar
+all: locvar vecsum
 
-locvar:
+locvar: locvar.cpp
+	g++ -Wall -O2 -Wno-unused-result -fopenmp $@.cpp -o $@
+
+vecsum: vecsum.cpp
 	g++ -Wall -O2 -Wno-unused-result -fopenmp $@.cpp -o $@
 
 clean:
---
+++ vecsum.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <omp.h>
+#include <cstdlib>
+
+using namespace std;
+
+void sumVec(int *one, int *two, int *out, int vecSize){
+	int init, step;
+
+	step = omp_get_num_threads();
+	init = omp_get_thread_num();
+	while(init < vecSize){
+		out[init] = one[init] + two[init];
+		init += step;
+	}
+}
+
+int main(int argc, char *argv[]){
+	int vecSize, *one, *two, *sum;
+
+	if(argc != 2)
+		return 0;
+
+	vecSize = atoi(argv[1]);
+	one = (int *) malloc(sizeof(int*) * vecSize);
+	two = (int *) malloc(sizeof(int*) * vecSize);
+	sum = (int *) malloc(sizeof(int*) * vecSize);
+
+	for(int i = 0; i < vecSize; i++){
+		one[i] = i;
+		two[i] = -i;
+	}
+
+	#pragma omp parallel
+	{
+	sumVec(one, two, sum, vecSize);
+	}
+	
+	for(int i = 0; i < vecSize; i++){
+		cout << sum[i] << " ";
+	}
+	cout << "\n";
+
+	return 0;
+}
